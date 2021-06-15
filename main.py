b7be284262a1cc56c9ea3427998b4800701eac0e
Enrico Ros <enrico.ros@gmail.com>
2021-06-14 21:34:12 -0700

    Cleanup

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ import tensorflow_hub as hub
 
 rounds_csv_file_names = ['data/tiger-rounds-6-9-2021.csv']
 
-# load CB transaction CSVs
-h_title = 'Title'
-h_industry = 'Organization Industries'
-h_desc = 'Organization Description'
 
-
-def load_cb_rounds_csv(csv_file_name):
+# data loader
+def cb_load_csv_rounds(csv_file_name):
+    header_title = 'Title'
+    header_industries = 'Organization Industries'
+    header_description = 'Organization Description'
     h_name = 'Organization Name'
     h_raise = 'Money Raised Currency (in USD)'
     headers_list = [
@@ -27,84 +26,65 @@ def load_cb_rounds_csv(csv_file_name):
         'Investor Names',
         'Lead Investors',
         'Pre-Money Valuation Currency (in USD)',
-        h_industry,
+        header_industries,
         'CB Rank (Funding Round)',
-        h_desc,
+        header_description,
     ]
     df = pd.read_csv(csv_file_name)
     df = df[headers_list]
-    df[h_title] = df.apply(lambda row: row[h_name] + ' (' + (str(round(row[h_raise] / 1E+06)) if not np.isnan(row[h_raise]) else '') + ' M)', axis=1)
+    df[header_title] = df.apply(lambda row: row[h_name] + ' (' + (str(round(row[h_raise] / 1E+06)) if not np.isnan(row[h_raise]) else '') + ' M)', axis=1)
     print('Total invested capital in', len(df), 'transactions:', round(np.sum(df[h_raise]) / 1E+07) / 100, ' $Bn')
-    return df
+    return df, header_title, header_industries
 
 
-df_cb = load_cb_rounds_csv(rounds_csv_file_names[0])
-
-# text embeddings for the 'industries'
-print('Encoding...')
+# sentence similarity, using USE from TF-Hub (instead of Sentence-Transformers, for instance)
 encoder_model = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 
 
-def text_to_embeds(model, text_list):
-    text_embeddings = model(text_list)
+def text_to_embeds(text_list):
+    text_embeddings = encoder_model(text_list)
     correlation = np.inner(text_embeddings, text_embeddings)
     return text_embeddings, correlation
 
 
-emb_industry, corr_industry = text_to_embeds(encoder_model, list(df_cb[h_industry]))
-emb_taglines, corr_taglines = text_to_embeds(encoder_model, list(df_cb[h_desc]))
-print('...done')
+# analysis from correlations matrices: group (sort rows, cols)
+def plot_correlation_sorted_df(df, corr_matrix, df_labels_header, chart_title):
+    def sort_df_by_correlation(_df, _corr_matrix):
+        curr_i = 0
+        sorted_i = [curr_i]
+        while len(sorted_i) < len(_df):
+            ith_corr = _corr_matrix[curr_i].copy()
+            while True:
+                closest_i = np.argmax(ith_corr)
+                if closest_i in sorted_i:
+                    ith_corr[closest_i] = -1
+                    continue
+                curr_i = closest_i
+                break
+            sorted_i.append(curr_i)
+        return _df.reindex(index=sorted_i), _corr_matrix[sorted_i, :][:, sorted_i]
 
+    def plot_corr_matrix_sns(_corr_matrix, labels, _title):
+        # mask = np.zeros_like(corr)
+        # mask[np.triu_indices_from(mask)] = True
+        sns.set(font_scale=1.0)
+        g = sns.heatmap(
+            data=_corr_matrix,
+            xticklabels=labels,
+            yticklabels=labels,
+            vmin=0,
+            vmax=np.max(_corr_matrix),
+            # mask=mask,
+            cmap="YlOrRd")
+        g.set_xticklabels(labels, rotation=90)
+        g.set_title(_title)
 
-# group DF by correlation
-def sort_df_by_correlation(df, corr_matrix):
-    curr_i = 0
-    sorted_i = [curr_i]
-    while len(sorted_i) < len(df):
-        ith_corr = corr_matrix[curr_i].copy()
-        while True:
-            closest_i = np.argmax(ith_corr)
-            if closest_i in sorted_i:
-                ith_corr[closest_i] = -1
-                continue
-            curr_i = closest_i
-            break
-        sorted_i.append(curr_i)
-    return df.reindex(index=sorted_i), corr_matrix[sorted_i, :][:, sorted_i]
-
-
-def plot_corr_matrix_sns(corr_matrix, labels, title):
-    # mask = np.zeros_like(corr)
-    # mask[np.triu_indices_from(mask)] = True
-    sns.set(font_scale=1.0)
-    g = sns.heatmap(
-        data=corr_matrix,
-        xticklabels=labels,
-        yticklabels=labels,
-        vmin=0,
-        vmax=np.max(corr_matrix),
-        # mask=mask,
-        cmap="YlOrRd")
-    g.set_xticklabels(labels, rotation=90)
-    g.set_title(title)
-
-
-def plot_correlation_sorted_df(df, corr_matrix, title):
     df, corr_matrix = sort_df_by_correlation(df, corr_matrix)
-    plot_corr_matrix_sns(corr_matrix, df[h_title], title)
+    plot_corr_matrix_sns(corr_matrix, df[df_labels_header], chart_title)
 
 
-plt.subplot()
-plot_correlation_sorted_df(df_cb, corr_industry, 'Tiger Global rounds 21.H1 - by startup industry similarity')
-
-plt.figure()
-plot_correlation_sorted_df(df_cb, corr_taglines, 'Tiger Global rounds 21.H1 - by startup taglines similarity')
-
-
-# subplot = plt.subplot()
-# plt.savefig('test.png')
-
-def plot_network(df, corr_matrix, layout_algo='lgl', corr_threshold=0.5):
+# Plot a Network Graph using the correlation matrix as edge weights, and the investment size as node weights
+def save_network_graph(df, corr_matrix, network_png, layout_algo='lgl', corr_threshold=0.5):
     graph = nx.Graph()
     mpl_color_map = plt.cm.get_cmap('autumn')  # summer, Wistia
 
@@ -163,12 +143,33 @@ def plot_network(df, corr_matrix, layout_algo='lgl', corr_threshold=0.5):
     # nx.draw_networkx_nodes(graph, pos, node_size=node_sizes, node_color='#ff8000')
     # nx.draw_networkx_labels(graph, pos, labels=node_labels, verticalalignment='bottom')
 
-    # Also use iGraph
+    # iGraph: create the network from NetworkX - seems stupid but it's easier
     gg = ig.Graph.from_networkx(graph)
-    ig.plot(gg, layout=gg.layout(layout_algo), bbox=(1920, 1280), margin=50)
+    # layout_algo: many, but the following are the best: 'lgl', 'graphopt', 'dh' (the latter is very slow)
+    ig.plot(gg, target=network_png, layout=gg.layout(layout_algo), bbox=(1920, 1280), margin=50)
 
 
-plot_network(df_cb, corr_industry, 'lgl', 0.2)  # good: lgl, graphopt, dh (slowest)
-plot_network(df_cb, corr_taglines, 'lgl', 0.2)  # good: lgl, graphopt, dh (slowest)
+### MAIN
+
+# process all input files
+for file_name, file_index in zip(rounds_csv_file_names, range(len(rounds_csv_file_names))):
+    # load file
+    print(f'Loading ${file_name}...')
+    df_cb, h_title, h_industries = cb_load_csv_rounds(file_name)
+
+    # compute sentence distance from the 'industry' column
+    print('Computing industry correlation matrix...')
+    emb_industry, corr_industry = text_to_embeds(list(df_cb[h_industries]))
+    print(' min:', np.min(corr_industry), ' max: ', np.max(corr_industry))
+
+    # plot the correlation matrix
+    print('Plotting industry correlation matrix...')
+    plt.subplot()
+    plot_correlation_sorted_df(df_cb, corr_industry, h_title, 'Tiger Global rounds 21.H1 - by startup industry similarity')
+    # plt.savefig('test.png')
+
+    # save a PNG file with the graph of rounds -- f'rounds_${file_index}.png'
+    print('Generating network graph of rounds, using the LGL algo')
+    save_network_graph(df_cb, corr_industry, None, 'lgl', 0.2)
 
 plt.show(block=True)
